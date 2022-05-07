44d6c8ef191688256a8bc3da5e64d6ea838cb963
Ryeker Schurtz <ryeker.schurtz@gmail.com>
2022-05-07 14:39:44 -0400

    add code

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+yum install python3 -y
+pip3 install venv
+mkdir /opt/metric_collector
+python3 -m venv /opt/metric_collector/venv
+source /opt/metric_collector/venv/bin/activate
+pip3 install psutil platform json argparse requests datetime os
+cp metric_collector.py /opt/metric_collector/
+cp metric_collector.service /etc/systemd/system/metric_collector.service
+chmod 0664 /etc/systemd/system/metric_collector.service
+systemctl daemon-reload
+systemctl start metric_collector
+systemctl enable metric_collector
+systemctl metric_collector status
